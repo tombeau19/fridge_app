63ffb45727dd874163a7fc3e656029623a04a409
tombeau19 <thomas.beauregard@gmail.com>
2017-10-26 17:00:48 -0400

    can list all fridges

--- lib/fridge_app.rb
+++ lib/fridge_app.rb
@@ -1,4 +1,12 @@
 require_relative '../config/environment'
 require_relative '../app/models/fridge'
 
+fridges = Fridge.all
+fridges.each do |fridge|
+    puts fridge.location
+end
+
+
+# Fridge.create()
+
 puts "test"
